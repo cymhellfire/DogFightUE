32efc05c3f8206bd0866f18ea3b0c0013186f67c
cymhellfire <cym-hellfire@qq.com>
2023-01-06 12:38:56 +0800

    [CW] Fix world bound cannot work.

--- Source/DogFightGame/Public/GameMode/TopDownStyleGameState.h
+++ Source/DogFightGame/Public/GameMode/TopDownStyleGameState.h
@@ -8,6 +8,7 @@ class DOGFIGHTGAME_API ATopDownStyleGameState : public ADogFightGameStateBase
 {
 	GENERATED_BODY()
 public:
+	ATopDownStyleGameState(const FObjectInitializer& ObjectInitializer);
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="TopDownGame")
 	FBox WorldBounds;
