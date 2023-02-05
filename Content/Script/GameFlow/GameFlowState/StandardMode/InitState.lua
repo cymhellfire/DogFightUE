e9934ccb112d3150a2eea781c3ab043685b23038
cymhellfire <cym-hellfire@qq.com>
2023-02-05 21:09:00 +0800

    [CW] Added basic timer service for lua usage.

--- Content/Script/GameFlow/GameFlowState/StandardMode/InitState.lua
+++ Content/Script/GameFlow/GameFlowState/StandardMode/InitState.lua
@@ -4,19 +4,6 @@ require "LuaIntegration.Common.UnrealUtils"
 ---@class InitState Initial state after player enter the game level until all players are loaded.
 local InitState = Class("GameFlow.GameFlowState.GameFlowStateLogicBase")
 
-local function LoopVfx(self)
-    for i = 1, 255 do
-        UE.UKismetSystemLibrary.Delay(self.OwnerState, 2)
-
-        local PosX = math.random(-400, 400)
-        local PosY = math.random(-400, 400)
-
-        local EffectId = (i % 5 == 0) and 1 or 0
-
-        UE.UCommonGameplayFunctionLibrary.SpawnGameEffectAtPos(EffectId, UE.FVector(PosX, PosY, 0), UE.FRotator(0, 0, 0))
-    end
-end
-
 function InitState:OnEnter()
     print("InitState: OnEnter")
     print("InitState: Instigator " .. self.OwnerState.CreateArgument.Instigator:GetName())
@@ -24,7 +11,12 @@ function InitState:OnEnter()
     GetGameService(GameServiceNameDef.LuaEventService):RegisterListener(UE.ELuaEvent.LuaEvent_ReadyPlayerCount,
         self, self.OnReadyPlayerCountChanged)
 
-    coroutine.resume(coroutine.create(LoopVfx), self)
+    self.VfxCount = 1
+    ---@type TimerService
+    local TimerService = GetGameService(GameServiceNameDef.TimerService)
+    if TimerService then
+        self.VfxTimer = TimerService:RegisterTimer(self, self.LoopVfxTimerExpired, 2, true)
+    end
 end
 
 function InitState:OnReadyPlayerCountChanged(InCount)
@@ -54,6 +46,26 @@ function InitState:OnReadyPlayerCountChanged(InCount)
         end
 
         self.OwnerState:Finish()
+    end
+end
+
+function InitState:LoopVfxTimerExpired()
+    local PosX = math.random(-400, 400)
+    local PosY = math.random(-400, 400)
+
+    local EffectId = (self.VfxCount % 5 == 0) and 1 or 0
+
+    UE.UCommonGameplayFunctionLibrary.SpawnGameEffectAtPos(EffectId, UE.FVector(PosX, PosY, 0), UE.FRotator(0, 0, 0))
+    self.VfxCount = self.VfxCount + 1;
+
+    -- Unregister after 5
+    if self.VfxCount > 5 then
+        ---@type TimerService
+        local TimerService = GetGameService(GameServiceNameDef.TimerService)
+        if TimerService then
+            TimerService:UnregisterTimer(self.VfxTimer)
+            self.VfxTimer = nil
+        end
     end
 end
 
---
+++ Content/Script/LuaIntegration/Services/TimerService.lua
@@ -0,0 +1,35 @@
+require "UnLua"
+
+---@class TimerService Service that manage all timer.
+local TimerService = Class("DogFight.Services.GameServiceBase")
+
+---Register a timer which can trigger once or multiple times.
+---@param InObj table Callback function container.
+---@param InCallback function Callback function to trigger.
+---@param Interval number How often this callback is triggered?
+---@param bIsLoop boolean Is the timer looping
+---@return FTimerHandle Handle of new register timer
+function TimerService:RegisterTimer(InObj, InCallback, Interval, bIsLoop)
+    if InObj == nil or InCallback == nil then
+        return
+    end
+
+    local TimerHandle = UE.UKismetSystemLibrary.K2_SetTimerDelegate({self, function()
+        -- Invoke callback
+        InCallback(InObj)
+    end}, Interval, bIsLoop or false)
+
+    return TimerHandle
+end
+
+---Clear specified timer by handle.
+---@param InHandle FTimerHandle Handle of timer to unregister.
+function TimerService:UnregisterTimer(InHandle)
+    if InHandle == nil then
+        return
+    end
+
+    UE.UKismetSystemLibrary.K2_ClearTimerHandle(self, InHandle)
+end
+
+return TimerService
